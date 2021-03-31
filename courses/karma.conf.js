026850bc915d38680789f55c86aa2acfb5f9dd20
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-31 10:52:08 -0500

    Nombre de proyecto, app-courses

--- courses/karma.conf.js
+++ courses/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/courses'),
+      dir: require('path').join(__dirname, './coverage/app-courses'),
       subdir: '.',
       reporters: [
         { type: 'html' },
